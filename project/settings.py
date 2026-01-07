4973685f309e26ab2265995c705381e5d8cc9418
abdelfatah hany <aaa5122003@gmail.com>
2026-01-07 03:36:24 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 # ===================================
 SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret-key')  # في PythonAnywhere استخدم env var
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')  # ضع أسماء دوميناتك هنا في env var
+DEBUG = False
+ALLOWED_HOSTS = ["*"]  # مؤقتًا
 
 # ===================================
 # Auth
