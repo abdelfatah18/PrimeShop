ca721fc794f75e6967d927a93862524658fba713
abdelfatah hany <aaa5122003@gmail.com>
2026-01-08 19:47:02 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -96,16 +96,18 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "project.wsgi.application"
+import dj_database_url
 
 # ======================
 # Database
 # ======================
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
 
 # ======================
 # Stripe (Test only)
