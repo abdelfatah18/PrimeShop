cefb9640c4d37265dcb316baafddfddf95ab7315
abdelfatah hany <aaa5122003@gmail.com>
2026-01-08 02:25:22 +0200

    Add Cloudinary storage configuration
    Configure Cloudinary storage settings for media files.

--- project/settings.py
+++ project/settings.py
@@ -155,3 +155,12 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
