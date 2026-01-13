713ae71a79f9ada966b21ae9882694611930f9a8
abdelfatah hany <aaa5122003@gmail.com>
2026-01-13 19:05:41 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 # ======================
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = Flase
 
 ALLOWED_HOSTS = [
     "grand-youthfulness-production.up.railway.app",
